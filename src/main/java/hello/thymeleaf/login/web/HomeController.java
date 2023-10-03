5d23420463fac3f6cbce657567553b8973002777
hanbee <sonhanbi1002@gmail.com>
2023-10-03 20:00:34 +0900

    home 화면 구성

--- src/main/java/hello/thymeleaf/login/web/HomeController.java
+++ src/main/java/hello/thymeleaf/login/web/HomeController.java
@@ -8,8 +8,8 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class HomeController {
 
-    @GetMapping("/")
+    @GetMapping("/home")
     public String home() {
-        return "redirect:/login/items";
+        return "login/home";
     }
 }
