a4c183459436dcb8842cf443186bf7642377d613
hanbee <sonhanbi1002@gmail.com>
2023-10-03 21:15:08 +0900

    쿠키 사용하여 로그인 상태 유지

--- src/main/java/hello/thymeleaf/login/web/HomeController.java
+++ src/main/java/hello/thymeleaf/login/web/HomeController.java
@@ -1,15 +1,37 @@
 package hello.thymeleaf.login.web;
 
+import hello.thymeleaf.login.domain.member.Member;
+import hello.thymeleaf.login.domain.member.MemberRepository;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.CookieValue;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Slf4j
 @Controller
+@RequiredArgsConstructor
 public class HomeController {
+    private final MemberRepository memberRepository;
 
-    @GetMapping("/home")
+//    @GetMapping("/home")
     public String home() {
         return "login/home";
+    }
+
+    @GetMapping("/home")
+    public String homeLogin(@CookieValue(name = "memberId", required = false) Long memberId, Model model) {
+        if (memberId == null) {
+            return "login/home";
+        }
+
+        Member loginMember = memberRepository.findById(memberId);
+        if (loginMember == null) {
+            return "login/home";
+        }
+
+        model.addAttribute("member", loginMember);
+        return "login/loginHome";
     }
 }
--- src/main/java/hello/thymeleaf/login/web/login/LoginController.java
+++ src/main/java/hello/thymeleaf/login/web/login/LoginController.java
@@ -10,6 +10,8 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletResponse;
 import javax.validation.Valid;
 
 @Slf4j
@@ -24,14 +26,18 @@ public class LoginController {
     }
 
     @PostMapping("/login")
-    public String login(@Valid @ModelAttribute("loginForm") LoginForm loginForm, BindingResult bindingResult) {
+    public String login(@Valid @ModelAttribute("loginForm") LoginForm loginForm, BindingResult bindingResult, HttpServletResponse response) {
         if (bindingResult.hasErrors()) {
             return "/login/login/loginForm";
         }
 
         try {
             Member loginMember = loginService.login(loginForm.getLoginId(), loginForm.getPassword());
-            // TODO 로그인 성공 처리
+
+            // 로그인 성공 처리
+            // 쿠키에 시간 정보를 주지 않으면 세션 쿠기 (브라우저 종료 시 모두 종료)
+            Cookie idCookie = new Cookie("memberId", String.valueOf(loginMember.getId()));
+            response.addCookie(idCookie);
 
             return "redirect:/home";
         } catch (IllegalArgumentException e) {
@@ -39,5 +45,17 @@ public class LoginController {
             bindingResult.reject("loginFail", "아이디 또는 비밀번호가 맞지 않습니다.");
             return "/login/login/loginForm";
         }
+    }
+
+    @PostMapping("/logout")
+    public String logout(HttpServletResponse response) {
+        expireCookie(response, "memberId");
+        return "redirect:/home";
+    }
+
+    private static void expireCookie(HttpServletResponse response, String cookieName) {
+        Cookie cookie = new Cookie(cookieName, null);
+        cookie.setMaxAge(0);
+        response.addCookie(cookie);
     }
 }
