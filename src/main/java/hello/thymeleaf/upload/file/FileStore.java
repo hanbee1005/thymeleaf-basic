261b07ff428839400fba8c5759cdf639ae783d36
hanbee <sonhanbi1002@gmail.com>
2023-10-25 07:33:50 +0900

    파일 업로드 예제 - filestore 생성

---
+++ src/main/java/hello/thymeleaf/upload/file/FileStore.java
@@ -0,0 +1,44 @@
+package hello.thymeleaf.upload.file;
+
+import hello.thymeleaf.upload.domain.UploadFile;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.UUID;
+
+@Component
+public class FileStore {
+    @Value("${file.dir}")
+    private String fileDir;
+
+    public String getFullPath(String filename) {
+        return fileDir + filename;
+    }
+
+    public UploadFile storeFile(MultipartFile multipartFile) throws IOException {
+        if (multipartFile.isEmpty()) return null;
+
+        String originalFilename = multipartFile.getOriginalFilename();
+        String storeFilename = createStoreFilename(originalFilename);
+
+        multipartFile.transferTo(new File(getFullPath(storeFilename)));
+
+        return new UploadFile(originalFilename, storeFilename);
+    }
+
+    private String createStoreFilename(String originalFilename) {
+        // 서버에 저장하는 파일명 = UUID
+        String uuid = UUID.randomUUID().toString();
+        String ext = extractExt(originalFilename);
+
+        return uuid + "." + ext;
+    }
+
+    private String extractExt(String filename) {
+        int pos = filename.lastIndexOf(".");
+        return filename.substring(pos + 1);
+    }
+}
