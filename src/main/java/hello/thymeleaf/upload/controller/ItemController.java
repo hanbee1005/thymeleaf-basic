6c0636db0e58217b43c8cdff329ad13bb5f603f4
hanbee <sonhanbi1002@gmail.com>
2023-10-27 07:28:15 +0900

    파일 업로드 예제 - upload 기능 구현

--- src/main/java/hello/thymeleaf/upload/controller/ItemController.java
+++ src/main/java/hello/thymeleaf/upload/controller/ItemController.java
@@ -1,14 +1,21 @@
 package hello.thymeleaf.upload.controller;
 
+import hello.thymeleaf.upload.domain.Item;
+import hello.thymeleaf.upload.domain.UploadFile;
 import hello.thymeleaf.upload.domain.UploadItemRepository;
 import hello.thymeleaf.upload.file.FileStore;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+import java.io.IOException;
+import java.util.List;
 
 @Slf4j
 @Controller
@@ -23,7 +30,27 @@ public class ItemController {
     }
 
     @PostMapping("/item/new")
-    public String saveItem(@ModelAttribute ItemForm itemForm, RedirectAttributes redirectAttributes) {
-        return "";
+    public String saveItem(@ModelAttribute ItemForm itemForm, RedirectAttributes redirectAttributes) throws IOException {
+        UploadFile storeAttachFile = fileStore.storeFile(itemForm.getAttachFile());
+        List<UploadFile> storeImageFiles = fileStore.storeFiles(itemForm.getImageFiles());
+
+        // 데이터베이스에 저장
+        Item item = new Item();
+        item.setItemName(itemForm.getItemName());
+        item.setAttachFile(storeAttachFile);
+        item.setImageFiles(storeImageFiles);
+
+        uploadItemRepository.save(item);
+
+        redirectAttributes.addAttribute("itemId", item.getId());
+
+        return "redirect:/items/{itemId}";
+    }
+
+    @GetMapping("/items/{id}")
+    public String items(@PathVariable Long id, Model model) {
+        Item item = uploadItemRepository.findById(id);
+        model.addAttribute("item", item);
+        return "/upload/item-view";
     }
 }
